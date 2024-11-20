0f49370ea6b943448a7b0b0df9af2b07757ebce0
Alkayami <alalwi322@gmail.com>
2024-11-20 07:05:45 +0700

    level 7

--- game.js
+++ game.js
@@ -1,8 +1,37 @@
-const buttonColor = ["red, blue, green, yellow"];
-let gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").on("click", function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+
+  console.log(userClickedPattern);
+
+  playSound(userChosenColor);
+});
 
 function nextSequence() {
-  const randomNumber = Math.floor(Math.random() * 4);
-  const randomChosenColor = buttonColor[randomNumber];
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  });
 }
